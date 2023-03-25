c3942d757676b7c506c928bf0a077310a40c6b9e
Nathash Kumar V <nathash.kumar@storeking.in>
2023-03-25 11:50:33 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const nodemailer = require('nodemailer');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index', { status: "send" });
+});
+
+app.post('/', async (req, res) => {
+
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  let mailOptions = {
+    from: 'nathash.kumar.92@gmail.com',
+    to: req.body.to,
+    subject: req.body.subject,
+    text: req.body.text
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.render('index', { status: 'error' });
+    } else {
+      res.render('index', { status: 'success' });
+    }
+  });
+});
+
+app.listen(3000, () => console.log('Server started...'));
